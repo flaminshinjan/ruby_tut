2bdbe884a315beb58b725239bb5e4e041d7d786e
flaminshinjan <srikpatra.2008@gmail.com>
2023-09-28 22:23:13 +0530

    first commit

---
+++ strings.rb
@@ -0,0 +1,10 @@
+puts 2 + 2
+puts "Hello world"
+puts "Hello world".upcase
+puts "2 + 2 = #{2 + 2}"
+puts "Hello world".class
+puts "Hello world".methods
+puts "Hello world".reverse
+puts "Hello " + "world"
+puts "Hello world".sub("world", "mars")
+puts "Hello, World".include?("World")
