b0355f8b09b3274ae3a32a5c97338037d98e7e38
flaminshinjan <srikpatra.2008@gmail.com>
2023-09-29 10:12:59 +0530

    Learning bss

--- strings.rb
+++ strings.rb
@@ -8,3 +8,9 @@ puts "Hello world".reverse
 puts "Hello " + "world"
 puts "Hello world".sub("world", "mars")
 puts "Hello, World".include?("World")
+puts "Hello, World"[7..11]
+puts "Hello, World".split(", ")
+puts "Hello, World".match(/He/)[0]
+puts "Hello, World".gsub("World", "Mars")
+puts 2+2
+puts 2.+(2)
